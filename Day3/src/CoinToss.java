c8b714f330aba463cf31de27603163323fc1e9bd
alohageck0 <royalfiish@gmail.com>
2016-01-03 14:53:15 -0800

    12/3/15

--- Day3/src/CoinToss.java
+++ Day3/src/CoinToss.java
@@ -1,7 +1,23 @@
 import java.util.Random;
 
 public class CoinToss {
-    public int tossACoin() {
+    public String tossACoin() {
         Random rand = new Random();
+        int toss = Math.abs(rand.nextInt()) % 2;
+        if (toss ==0){
+            return "Head";
+        }else {
+            return "Tail";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CoinToss game = new CoinToss();
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+
     }
 }
