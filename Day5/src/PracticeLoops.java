551b509bc0b6d4d6408669bc5b5ee4237c6e3d2b
Evgenii <royalfiish@gmail.com>
2016-01-05 18:49:35 -0800

    01/5/16

--- Day5/src/PracticeLoops.java
+++ Day5/src/PracticeLoops.java
@@ -3,7 +3,21 @@
  */
 public class PracticeLoops {
 
-    public void practiceWhileLoop(){
-
+    public static void practiceWhileLoop() {
+        int x = 0;
+        while (x < 5) {
+            System.out.println("Value of x is " + x);
+            x++;
+        }
+    }
+    public static void practiceDoWhileLoop(){
+        int x = 0;
+        do {
+            System.out.println("Value of x is "+ x);
+            x++;
+        }while (x<5);
+    }
+    public static void main(String[] args) {
+        practiceDoWhileLoop();
     }
 }
