587bab9bd9cb041b8115cf0957710cf02e3692c3
alohageck0 <royalfiish@gmail.com>
2016-01-06 21:45:00 -0800

    12/6/15

--- Day6/src/Solution.java
+++ Day6/src/Solution.java
@@ -6,10 +6,10 @@ public class Solution {
         Scanner s = new Scanner(System.in);
         String rr = "#";
         int n = s.nextInt();
-        for (int x = n; x > 0; x--) {
+        for (int x = n-1; x >= 0; x--) {
 
             System.out.println(rr.join("", Collections.nCopies(x, " "))
-                    + rr.join("", Collections.nCopies(n - x + 1, rr)));
+                    + rr.join("", Collections.nCopies(n - x, rr)));
         }
     }
 }
