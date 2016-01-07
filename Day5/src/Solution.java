144a9609906176c6dee8d901f1880b1b690e2162
alohageck0 <royalfiish@gmail.com>
2016-01-06 20:08:03 -0800

    12/6/15

--- Day5/src/Solution.java
+++ Day5/src/Solution.java
@@ -1,39 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public static int getNumberOfTestcases() {
-        Scanner numberOfTestcases = new Scanner(System.in);
-        return numberOfTestcases.nextInt();
-    }
-
     public static void main(String[] args) {
-        Scanner testcase = new Scanner(System.in);
-        ArrayList<String> results = new ArrayList<>();
-        int numberOfTests = testcase.nextInt();
-//        System.out.println(numberOfTests);
-        int x;
-        for (x = 0; x < numberOfTests; x++) {
-            Scanner strin = new Scanner(System.in);
-            int a = strin.nextInt();
-            int b = strin.nextInt();
-            int N = strin.nextInt();
-            int sum = 0;
-            StringBuilder result = new StringBuilder();
-            for (int y = 0; y < N; y++) {
-                if (y == 0) {
-                    sum = a + b;
-                    result.append(sum);
-                } else {
-                    sum = (int) (sum + Math.pow(2, y) * b);
-                    result.append(" " + sum);
-                }
-            }
-            results.add(result.toString());
-        }
-        for (x = 0; x < numberOfTests; x++) {
-            System.out.println(results.get(x));
+        Scanner s = new Scanner(System.in);
+        for (int i = 0, c = s.nextInt(); i < c; i++) {
+            for (int k = 0, a = s.nextInt(), b = s.nextInt(), n = s.nextInt(); k < n; k++)
+                System.out.print((a += b * (int) Math.pow(2, k)) + " ");
+            System.out.println();
         }
     }
 }
