915ea66d7dbebd3fa649a7225586f2da69d2bfa6
alohageck0 <royalfiish@gmail.com>
2016-01-09 17:53:37 -0800

    12/9/15

--- Day9/src/Solution.java
+++ Day9/src/Solution.java
@@ -1,13 +1,21 @@
+import java.io.*;
+import java.util.*;
+
 public class Solution {
-    public static int globalCommonDivisor(int x, int y) {
-        if (y % x == 0) {
-            return x;
-        } else {
-            return globalCommonDivisor(x, y - (y%x));
-        }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        //Take Input
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int gcd = find_gcd(a, b);
+        System.out.println(gcd);
     }
 
-    public static void main(String[] args) {
-        System.out.println(globalCommonDivisor(3, 4));
+    static int find_gcd(int a, int b) {
+        //Write the base condition
+        if (b != 0) {
+            return find_gcd(b, a % b);
+        }
+        return a;
     }
 }
