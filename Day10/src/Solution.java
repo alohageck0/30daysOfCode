14edb3574bc738c3dde715f6db4ba76638a0c854
alohageck0 <royalfiish@gmail.com>
2016-01-10 22:02:18 -0800

    12/10/15

--- Day10/src/Solution.java
+++ Day10/src/Solution.java
@@ -1,11 +1,37 @@
 import java.util.Scanner;
 
 public class Solution {
+
+    public static String toBinary(int decimal, StringBuilder result) {
+        while (decimal != 0 || decimal != 1) {
+            int reminder = decimal % 2;
+            result.append(reminder);
+            return toBinary(decimal / 2, result);
+        }
+        if (decimal == 0) {
+            result.append(1);
+        } else {
+            result.append(0);
+        }
+        return result.reverse().toString();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int n = 0; n < sc.nextInt(); n++) {
-            int decimal = sc.nextInt();
-            System.out.println(decimal);
+        int testcases = sc.nextInt();
+        for (int n = 0; n < testcases; n++) {
+            StringBuilder result = new StringBuilder();
+            int testcase = sc.nextInt();
+            while (testcase != 0 || testcase != 1) {
+                result.append(testcase % 2);
+                testcase = testcase/2;
+            }
+             if (testcase == 0) {
+                result.append(1);
+            } else {
+                result.append(0);
+            }
+            System.out.println(result.reverse().toString());
         }
     }
 }
