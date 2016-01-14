7dd769bdbf4202aabb041d75975cab552ad18944
alohageck0 <royalfiish@gmail.com>
2016-01-13 22:19:58 -0800

    01/13/16

--- Day13/src/MyBook.java
+++ Day13/src/MyBook.java
@@ -1,2 +1,14 @@
-public class MyBook {
+public class MyBook extends Book {
+    public int price;
+
+    public MyBook(String title, String author, int bookPrice) {
+        super(title, author);
+        this.price = bookPrice;
+    }
+
+    public void display() {
+        System.out.println("Title: " + this.title);
+        System.out.println("Author: " + this.author);
+        System.out.println("Price: " + this.price);
+    }
 }
