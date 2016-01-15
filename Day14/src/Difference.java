5762354cb23953869e3747c99ce952c2a200a99b
alohageck0 <royalfiish@gmail.com>
2016-01-14 18:38:19 -0800

    01/14/16

---
+++ Day14/src/Difference.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    public Difference(int[] inputElements) {
+        this.elements = inputElements;
+    }
+
+    public int computeDifference() {
+        return maximumDifference;
+    }
+}
