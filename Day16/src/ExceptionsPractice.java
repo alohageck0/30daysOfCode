27244af969dc1d09f46e83c7a306587d484611ce
alohageck0 <royalfiish@gmail.com>
2016-01-17 18:00:04 -0800

    01/17/16 Exceptions

---
+++ Day16/src/ExceptionsPractice.java
@@ -0,0 +1,11 @@
+public class ExceptionsPractice {
+    public static void main(String[] args) {
+        try {
+            int[] c = new int[6];
+            System.out.println("Element 6 at index 5 = " + c[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception thrown " + e);
+        }
+        System.out.println("Finally finished try-catch");
+    }
+}
