30ba6967a4ace6aed64323ca734c51d11486577c
alohageck0 <royalfiish@gmail.com>
2016-01-17 16:53:48 -0800

    01/17/16 Insert method done

--- Day15/src/Solution.java
+++ Day15/src/Solution.java
@@ -16,13 +16,17 @@ class Solution {
     public static Node insert(Node head, int data) {
         //Complete this method
         if (head == null) {
-            Node newNode = new Node(data);
-            return newNode;
-        } else {
-            Node lastNode = new Node(data);
-            head.next = lastNode;
-            return lastNode;
+            head = new Node(data);
+            return head;
         }
+        Node temp = new Node(data);
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = temp;
+        return head;
+
     }
 
     public static void display(Node head) {
