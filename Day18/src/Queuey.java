21b45b0bf3d597efa0ee97a41382b20acd55a697
alohageck0 <royalfiish@gmail.com>
2016-01-18 20:23:19 -0800

    01/18/16 Queue and stacks

---
+++ Day18/src/Queuey.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Queuey {
+    LinkedList queue;
+
+    //
+    public Queuey() {
+        queue = new LinkedList();
+    }
+
+    //Is our queue empty
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public void enqueue(String n) {
+        queue.addLast(n);
+    }
+
+    public String dequeue() {
+        return (String) queue.removeFirst();
+    }
+
+    public String peek() {
+        return (String) queue.get(0);
+    }
+
+    public static void main(String[] args) {
+//        Queuey numberQueue = new Queuey();
+//        numberQueue.enqueue(5);
+//        numberQueue.enqueue(7);
+//        numberQueue.enqueue(6);
+//        System.out.println("First out " + numberQueue.dequeue());
+//        System.out.println("Peek at ceond out " + numberQueue.peek());
+//        System.out.println("Second out " + numberQueue.dequeue());
+//        System.out.println("Third out " + numberQueue.dequeue());
+        Queuey stringQueue = new Queuey();
+        stringQueue.enqueue("Hi");
+        stringQueue.enqueue("there");
+        System.out.print(stringQueue.dequeue() + " ");
+        System.out.print(stringQueue.dequeue() + ". ");
+
+    }
+}
