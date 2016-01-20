2dd0a916add2bdb7ada2c427c71642bda52d5a21
Evgenii <royalfiish@gmail.com>
2016-01-20 13:05:45 -0800

    01/20/16 solution

--- Day20/src/Solution.java
+++ Day20/src/Solution.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String delimiters = "![,?.\\_'@+] ";
+        String s = sc.nextLine();
+        StringTokenizer result = new StringTokenizer(s, delimiters);
+        System.out.println(result.countTokens());
+        for (int i = 0; i <= result.countTokens(); i++) {
+            System.out.println(result.nextToken());
+        }
+    }
 }
