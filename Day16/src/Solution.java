5a086a233f29c0caa6cfe7ee4fbcba007997d6b8
alohageck0 <royalfiish@gmail.com>
2016-01-20 21:31:10 -0800

    01/20/16 Solution Day16 in process

--- Day16/src/Solution.java
+++ Day16/src/Solution.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<Integer> input = new ArrayList<>();
+        while (N-- != 0) {
+            input.add(sc.nextInt());
+        }
+        Object[] sorted = input.toArray();
+        Arrays.sort(sorted);
+        int diff = Math.abs((int) sorted[1] - (int) sorted[0]);
+        for (int i = 1; i < sorted.length - 1; i++) {
+            if (((int) sorted[i + 1] - (int) sorted[i]) < diff) {
+                diff = (int) sorted[i + 1] - (int) sorted[i];
+            }
+        }
+        for (int i = 0; i < sorted.length; i++) {
+            int newDiff = (int) sorted[i + 1] - (int) sorted[i];
+            if (diff == newDiff) {
+                System.out.print(sorted[i] + " " + sorted[i + 1]);
+            }
+        }
     }
 }
