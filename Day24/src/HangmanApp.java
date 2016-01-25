cf9943f4b77d10115ff7037dc2fa111e41a42dbb
alohageck0 <royalfiish@gmail.com>
2016-01-24 19:03:07 -0800

    01/24/16 Solution delete duplicates

---
+++ Day24/src/HangmanApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HangmanApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Hangman! I will pick a word and you will try to guess it, if you guess wrong 6 times, ...");
+        System.out.println();
+        System.out.println("I have picked my word. Below is a picture, and below that is your current guess");
+        boolean doYouWanttoPlay = true;
+        //keep playing
+        System.out.println();
+
+    }
+
+}
