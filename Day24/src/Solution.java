290a1fc217c0a4e311f71305008a312602bf2c08
alohageck0 <royalfiish@gmail.com>
2016-01-24 19:21:32 -0800

    01/24/16 Solution delete duplicates

--- Day24/src/Solution.java
+++ Day24/src/Solution.java
@@ -13,8 +13,17 @@ class Node {
 
 class Solution {
     public static Node removeDuplicates(Node head) {
-
-
+        if (head == null) {
+            return head;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            if (temp.data == temp.next.data) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
         return head;
     }
 
