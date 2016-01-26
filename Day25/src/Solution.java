bc559ce97ffd09309aa1a368571c692af8478475
alohageck0 <royalfiish@gmail.com>
2016-01-25 23:03:25 -0800

    01/25/16 Solution in process

--- Day25/src/Solution.java
+++ Day25/src/Solution.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 
 public class Solution {
-    public static String ifPrime(int number) {
-        if (number % 2 == 0 && number != 2) {
+    public static String ifPrime(long number) {
+        if (number == 2 || number == 3 || number == 5 || number == 7 || number == 11) {
+            return "Prime";
+        } else if (number % 2 == 0 || number == 1 || number == 0) {
             return "Not prime";
+        } else {
+            for (int i = 3; i < Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    return "Not prime";
+                } else {
+                    return "Prime";
+                }
+            }
         }
-        return "Prime";
+        return null;
     }
 
     public static void main(String[] args) {
