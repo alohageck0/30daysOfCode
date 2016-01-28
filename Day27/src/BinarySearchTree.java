333f5aad155f36155aec52b15e624710d409e21b
alohageck0 <royalfiish@gmail.com>
2016-01-27 18:46:31 -0800

    01/27/16

--- Day27/src/BinarySearchTree.java
+++
@@ -1,8 +0,0 @@
-public class BinarySearchTree {
-    public static void main(String[] args) throws Exception {
-        EmptyBST e = new EmptyBST();
-        NonEmptyBST n = new NonEmptyBST(5);
-        Testers.checkIsEmpty(e);
-        Testers.checkIsEmpty(n);
-    }
-}
--- Day27/src/Tree.java
+++
@@ -1,9 +0,0 @@
-public interface Tree<D extends Comparable> {
-    boolean isEmpty();
-
-    int cardinality();
-
-    boolean member(D elt);
-
-    NonEmptyBST<D> add(D elt);
-}
