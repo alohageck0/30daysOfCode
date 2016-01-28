6b8f212f363d8d601529f5c1423636af887eb819
alohageck0 <royalfiish@gmail.com>
2016-01-27 19:09:55 -0800

    01/27/16

---
+++ Day27-1/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("5");
+        System.out.println("6 4");
+        System.out.println("-1 -2 0 4 5 6");
+        System.out.println("4 2");
+        System.out.println("3 4 -1 0");
+        System.out.println("7 3");
+        System.out.println("0 1 -1 3 4 5 8");
+        System.out.println("5 1");
+        System.out.println("-1 -2 -3 0 1");
+        System.out.println("8 5");
+        System.out.println("0 0 -1 3 5 6 9 10");
+    }
+}
