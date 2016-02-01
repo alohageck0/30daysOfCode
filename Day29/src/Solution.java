03c617d80d59ebed285fb9dcecd377c305c830c2
alohageck0 <royalfiish@gmail.com>
2016-01-31 19:11:48 -0800

    01/31/16

--- Day29/src/Solution.java
+++ Day29/src/Solution.java
@@ -1,5 +1,22 @@
-public class Solution {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Solution {
+    public static boolean ifFunny(String strin) {
+        boolean result = true;
+        int n = strin.length()-1;
+        for (int i = 1; i < n; i++) {
+            if (Math.abs((int) strin.charAt(i + 1) - (int) strin.charAt(i)) != Math.abs((int) strin.charAt(n - i) - (int) strin.charAt(n - i - 1))) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            System.out.println(ifFunny(sc.next()));
+        }
     }
 }
--- test/src/test.java
+++ test/src/test.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class test {
+    public static boolean ifFunny(String strin) {
+        boolean result = true;
+        int n = strin.length()-1;
+        for (int i = 0; i < n; i++) {
+
+            System.out.println(strin.charAt(i+1) + " " + strin.charAt(i));
+            System.out.println(strin.charAt(n - i) + " " + strin.charAt(n - i - 1));
+            System.out.println();
+//            if (Math.abs((int) strin.charAt(i+1) - (int) strin.charAt(i)) != Math.abs((int) strin.charAt(n - i) - (int) strin.charAt(n - i - 1))) {
+//                result = false;
+//            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        int N = 4;
-        while (N-->0){
-            System.out.println(N);
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            System.out.println(ifFunny(sc.next()));
         }
     }
 }
